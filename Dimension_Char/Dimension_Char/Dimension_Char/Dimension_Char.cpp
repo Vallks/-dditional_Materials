077469f4b31e004c15bb1959bf03837bb7c5a8ed
Валентин Командиров <komandirov.v@mail.ru>
2025-02-15 18:07:15 +0300

    Размерность Char

---
+++ Dimension_Char/Dimension_Char/Dimension_Char/Dimension_Char.cpp
@@ -0,0 +1,40 @@
+﻿// Dimension_Char.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    char c = 88;
+
+    std::cout << "c = " << +c << std::endl;
+    std::cout << "sizeof(c) = " << sizeof(c) << std::endl;
+    std::cout << "char max =  " << +std::numeric_limits<char>::max() << std::endl;
+    std::cout << std::endl;
+
+    signed char sc = -10;
+
+    std::cout << "sc = " << +sc << std::endl;
+    std::cout << "sizeof(sc) = " << sizeof(sc) << std::endl;
+    std::cout << "signed char max =  " << +std::numeric_limits<signed char>::max() << std::endl;
+    std::cout << std::endl;
+
+    unsigned char uc = 14;
+
+    std::cout << "uc = " << +c << std::endl;
+    std::cout << "sizeof(uc) = " << sizeof(uc) << std::endl;
+    std::cout << "unsigned char max =  " << +std::numeric_limits<unsigned char>::max() << std::endl;
+    std::cout << std::endl;
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
